f1735d5d6d274f1331c0edbedeaaf73660f9eea1
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2024-02-05 12:11:27 +0100

    Create and style about section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.navbar');
+	const allNavItems = document.querySelectorAll('.nav-link');
+	const navList = document.querySelector('.navbar-collapse');
 
-document.addEventListener('DOMContentLoaded', function(){
-    const nav = document.querySelector('.navbar');
-    function addShadow() {
-        if(window.scrollY >= 300){
-            nav.classList.add('shadow-bg')
-        } else {
-            nav.classList.remove('shadow-bg')
-        }
-    }
-window.addEventListener('scroll', addShadow)
+	function addShadow() {
+		if (window.scrollY >= 300) {
+			nav.classList.add('shadow-bg');
+		} else {
+			nav.classList.remove('shadow-bg');
+		}
+	}
 
-})
+	allNavItems.forEach((link) =>
+		link.addEventListener('click', () => {
+			navList.classList.remove('show');
+		})
+	);
+
+	window.addEventListener('scroll', addShadow);
+});
